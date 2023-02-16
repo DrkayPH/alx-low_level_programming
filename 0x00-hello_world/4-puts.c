39bbec432254a38a565b21edb931e8da5c2b9ef3
DrkayPH <oghenekarouche@gmail.com>
2023-02-16 11:50:11 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - serves as the  Entry point point of any code
+ *Return: - will be zero indicating Success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
