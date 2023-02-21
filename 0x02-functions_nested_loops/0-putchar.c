928b051504e8ad154b852e711f77368b07019d21
DrkayPH <oghenekarouche@gmail.com>
2023-02-21 11:54:59 -0800

    main header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include <main.h>
 
-#include "holberton.h"
-
 /**
  * main - print _putchar, followed by new line
  * Return: 0
@@ -9,15 +7,6 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	_putchar("_putchar \n");
 	return (0);
 }
